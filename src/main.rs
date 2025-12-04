13364d8693145dcdf1956bd7207df0e4ccbf164b
savannstm <savannstm@gmail.com>
2025-12-04 16:41:00 +0500

    v4.0.0: Update tp `rpgmad-lib` 4.0.0 and bring feature to encrypt archives

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,91 @@
+#![warn(clippy::all, clippy::pedantic)]
+#![allow(clippy::needless_doctest_main)]
+#![allow(clippy::cast_possible_truncation)]
+#![allow(clippy::cast_possible_wrap)]
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::deref_addrof)]
+
 use anyhow::{Context, Result, bail};
-use clap::{Parser, value_parser};
-use rpgmad_lib::Decrypter;
+use clap::{Parser, Subcommand};
+use rpgmad_lib::{ArchiveEntry, Decrypter, Engine};
 use std::{
+    borrow::Cow,
+    ffi::OsStr,
     fs::{create_dir_all, read, read_dir, write},
+    io::stdin,
     path::PathBuf,
     time::Instant,
 };
+use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
 #[command(
-    about = "Extract encrypted .rgss RPG Maker archives.",
+    about = "A tool to extract encrypted .rgss RPG Maker archives and encrypt RPG Maker assets back to archives.",
     version,
     term_width = 120
 )]
 struct Cli {
-    /// Path to the .rgss file or directory containing it.
-    #[arg(short, long, value_name = "INPUT_PATH", value_parser = value_parser!(PathBuf), default_value = "./", hide_default_value = true)]
-    input_path: PathBuf,
-
-    /// Output directory. Defaults to `input_path` if not set.
-    #[arg(short, long, value_name = "OUTPUT_PATH", value_parser = value_parser!(PathBuf))]
-    output_path: Option<PathBuf>,
+    /// Subcommand: encrypt or decrypt
+    #[command(subcommand)]
+    command: Command,
 }
 
-fn main() -> Result<()> {
-    let start_time = Instant::now();
-    let mut cli = Cli::parse();
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Decrypt an .rgss archive
+    Decrypt {
+        /// Path to the .rgss file or directory containing it.
+        #[arg(
+            short,
+            long,
+            value_name = "INPUT_PATH",
+            default_value = "./",
+            hide_default_value = true
+        )]
+        input_path: PathBuf,
 
-    if !cli.input_path.exists() {
+        /// Output directory. Defaults to `input_path` if not set.
+        #[arg(short, long, value_name = "OUTPUT_PATH")]
+        output_path: Option<PathBuf>,
+    },
+
+    /// Encrypt RPG Maker Data/Graphics assets to an archive
+    Encrypt {
+        /// Path to directory containing Data/Graphics directories
+        #[arg(
+            short,
+            long,
+            value_name = "INPUT_PATH",
+            default_value = "./",
+            hide_default_value = true
+        )]
+        input_path: PathBuf,
+
+        /// Path to write output .rgss file
+        #[arg(short, long, value_name = "OUTPUT_PATH")]
+        output_path: Option<PathBuf>,
+
+        /// Engine to produce proper archive
+        #[arg(short, long, value_name = "ENGINE", value_parser = ["xp", "vx", "vxace"])]
+        engine: String,
+    },
+}
+
+const ENCRYPT_DIRS: &[&str] = &["Graphics", "Data"];
+
+fn decrypt_path(
+    mut input_path: PathBuf,
+    output_path: Option<PathBuf>,
+) -> Result<()> {
+    if !input_path.exists() {
         bail!("Input path does not exist.");
     }
 
-    let output_path = cli.output_path.unwrap_or_else(|| {
-        if cli.input_path.is_file() {
-            cli.input_path.parent().unwrap().to_path_buf()
+    let output_path = output_path.unwrap_or_else(|| {
+        if input_path.is_file() {
+            input_path.parent().unwrap().to_path_buf()
         } else {
-            cli.input_path.clone()
+            input_path.clone()
         }
     });
 
@@ -43,27 +93,27 @@ fn main() -> Result<()> {
         bail!("Output path does not exist.");
     }
 
-    if cli
-        .input_path
+    // Detect .rgss file inside folder
+    if input_path
         .extension()
-        .and_then(|extension| extension.to_str())
-        .is_none_or(|extension| !extension.starts_with("rgss"))
+        .and_then(OsStr::to_str)
+        .is_none_or(|ext| !ext.starts_with("rgss"))
     {
-        cli.input_path = read_dir(&cli.input_path)?
+        input_path = read_dir(&input_path)?
             .flatten()
             .find(|entry| {
                 entry
                     .path()
                     .extension()
-                    .and_then(|extension| extension.to_str())
-                    .is_some_and(|extension| extension.starts_with("rgss"))
+                    .and_then(OsStr::to_str)
+                    .is_some_and(|ext| ext.starts_with("rgss"))
             })
             .map(|entry| entry.path())
             .context("No .rgss archive found in the directory.")?;
     }
 
     let mut decrypter = Decrypter::new();
-    let archive_data = read(&cli.input_path)?;
+    let archive_data = read(&input_path)?;
     let decrypted_files = decrypter.decrypt(&archive_data)?;
 
     for file in decrypted_files {
@@ -74,7 +124,109 @@ fn main() -> Result<()> {
             create_dir_all(parent)?;
         }
 
-        write(output_file_path, file.content)?;
+        write(output_file_path, file.data)?;
+    }
+
+    Ok(())
+}
+
+fn encrypt_path(
+    input_path: &PathBuf,
+    output_path: Option<&PathBuf>,
+    engine: &str,
+) -> Result<()> {
+    if !input_path.exists() {
+        bail!("Input path does not exist.");
+    }
+
+    let output_path = output_path.unwrap_or(input_path);
+    let output_file = output_path.join("Game").with_extension(match engine {
+        "xp" => "rgssad",
+        "vx" => "rgss2a",
+        "vxace" => "rgss3a",
+        _ => unreachable!(),
+    });
+
+    if output_file.exists() {
+        let filename = output_file.file_name().unwrap();
+        let mut input = String::with_capacity(4);
+
+        println!(
+            "{} already exists. Overwrite it? Input 'Y' to continue.",
+            filename.display()
+        );
+        stdin().read_line(&mut input)?;
+
+        if input.trim_end() != "Y" {
+            return Ok(());
+        }
+    }
+
+    let mut archive_entries: Vec<ArchiveEntry> = Vec::with_capacity(128);
+
+    for dir in ENCRYPT_DIRS {
+        let subdir = input_path.join(dir);
+
+        if !subdir.is_dir() || !subdir.exists() {
+            continue;
+        }
+
+        let entries = WalkDir::new(&subdir).into_iter().flatten();
+
+        for entry in entries {
+            let path = entry.path();
+
+            if !path.is_file() {
+                continue;
+            }
+
+            let relative_path = path.strip_prefix(&subdir).unwrap();
+
+            let data = read(path)?;
+            archive_entries.push(ArchiveEntry {
+                path: Cow::Owned(
+                    relative_path.as_os_str().as_encoded_bytes().to_vec(),
+                ),
+                data,
+            });
+        }
+    }
+
+    if archive_entries.is_empty() {
+        bail!(
+            "No valid directories found (Data, Graphics). Nothing to encrypt."
+        );
+    }
+
+    let mut decrypter = Decrypter::new();
+    let archive = decrypter.encrypt(
+        &archive_entries,
+        match engine {
+            "xp" | "vx" => Engine::Older,
+            "vxace" => Engine::VXAce,
+            _ => unreachable!(),
+        },
+    );
+
+    write(output_file, archive)?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let start_time = Instant::now();
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Decrypt {
+            input_path,
+            output_path,
+        } => decrypt_path(input_path, output_path)?,
+
+        Command::Encrypt {
+            input_path,
+            output_path: output_file,
+            engine,
+        } => encrypt_path(&input_path, output_file.as_ref(), &engine)?,
     }
 
     println!("Elapsed: {:.2}s", start_time.elapsed().as_secs_f32());
